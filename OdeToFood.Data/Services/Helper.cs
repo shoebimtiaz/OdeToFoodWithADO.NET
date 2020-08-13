0382d4ee7040f48d6f836a7ce144e4739820f24a
shoebimtiaz <shoebimtiaz@gmail.com>
2020-08-13 11:17:20 -0500

    first commit

---
+++ OdeToFood.Data/Services/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public static class Helper
+    {
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+    }
+}
---
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,88 @@
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        IRestaurantData db;
+
+        public RestaurantsController(IRestaurantData db)
+        {
+            this.db = db;
+        }
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+        public ActionResult Details(int id)
+        {
+            var model = db.Get(id);
+  
+            if(model == null)
+            {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Add(restaurant);
+                TempData["Message"] = "You have created the restaurant!";
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+            return View(restaurant);
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            var model = db.Get(id);
+            return View(model);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Update(restaurant);
+                TempData["Message"] = "You have saved the restaurant!";
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+            return View(restaurant);
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            var model = db.Get(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection form)
+        {
+            db.Delete(id);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
